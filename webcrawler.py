7d40104a25f7bbec62bbc1cd37a07265691c639b
George Kurobara Benjamin <msdcconnect@gmail.com>
2019-08-19 18:51:17 +0100

    Remove unused function

--- webcrawler.py
+++ webcrawler.py
@@ -1,27 +1,5 @@
 import urllib.request
 
-def get_page_mock(url):
-      if url == "http://www.udacity.com/cs101x/index.html":
-            return ('<html> <body> This is a test page for learning to crawl! '
-            '<p> It is a good idea to '
-            '<a href="http://www.udacity.com/cs101x/crawling.html">learn to '
-            'crawl</a> before you try to  '
-            '<a href="http://www.udacity.com/cs101x/walking.html">walk</a> '
-            'or  <a href="http://www.udacity.com/cs101x/flying.html">fly</a>. '
-            '</p> </body> </html> ')
-      elif url == "http://www.udacity.com/cs101x/crawling.html":
-            return ('<html> <body> I have not learned to crawl yet, but I '
-            'am quite good at '
-            '<a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.'
-            '</body> </html>')
-      elif url == "http://www.udacity.com/cs101x/walking.html":
-            return ('<html> <body> I cant get enough '
-            '<a href="http://www.udacity.com/cs101x/index.html">crawling</a>! '
-            '</body> </html>')
-      elif url == "http://www.udacity.com/cs101x/flying.html":
-            return ('<html> <body> The magic words are Squeamish Ossifrage! '
-            '</body> </html>')
-
 
 def get_page(url):
     try:
